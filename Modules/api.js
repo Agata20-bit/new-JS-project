7f8ec6ac2fa8fad899d8db2580e54b88126da7e1
Agata <agat.2012@yandex.ru>
2025-12-22 21:37:23 +0500

    API 22.12.25

---
+++ Modules/api.js
@@ -0,0 +1,9 @@
+const host = "https://wedev-api.sky.pro/api/v1/Agata20-bit"
+
+export const fetchComments = () => {
+  return fetch(host + "/comments")
+    .then(res => res.json())
+    .then(data => {
+      return data;
+    });
+};
--- strict.js
+++ strict.js
@@ -1,9 +1,11 @@
 import { comments } from './Modules/comments.js';
 import { renderComments } from './Modules/render-comments.js';
 import { handleLikes, setupReplyHandler, setupAddCommentHandler } from './Modules/event-handlers.js';
+import { fetchComments } from './Modules/comments.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  renderComments(comments);
+document.addEventListener('DOMContentLoaded', async () => {
+  const commentsFromAPI = await fetchComments();
+  renderComments(commentsFromAPI);
   handleLikes();
   setupReplyHandler();
   setupAddCommentHandler();
