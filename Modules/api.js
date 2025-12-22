4c726b1cff801d01d205f0fd720ec0e47ebee5e2
Agata <agat.2012@yandex.ru>
2025-12-22 21:45:21 +0500

    API22/12/25

--- Modules/api.js
+++ Modules/api.js
@@ -1,9 +1,9 @@
-const host = "https://wedev-api.sky.pro/api/v1/Agata20-bit"
+const host = 'https://wedev-api.sky.pro/api/v1/Agata20-bit';
 
 export const fetchComments = () => {
-  return fetch(host + "/comments")
-    .then(res => res.json())
-    .then(data => {
-      return data;
-    });
+    return fetch(host + '/comments')
+        .then((res) => res.json())
+        .then((data) => {
+            return data;
+        });
 };
